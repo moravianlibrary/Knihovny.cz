f31bcd0307ed6df8caaab6f2feb5397f46d26f13
Moravec Josef <josef.moravec@mzk.cz>
2020-03-17 11:55:16 +0000

    Add widgets (#2)

---
+++ module/KnihovnyCz/src/KnihovnyCz/ContentBlock/UserList.php
@@ -0,0 +1,85 @@
+<?php
+
+/**
+ * Class UserList
+ *
+ * PHP version 7
+ *
+ * Copyright (C) Moravian Library 2019.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * @category VuFind
+ * @package  KnihovnyCz\ContentBlock
+ * @author   Josef Moravec <moravec@mzk.cz>
+ * @license  https://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     https://knihovny.cz Main Page
+ */
+
+namespace KnihovnyCz\ContentBlock;
+
+class UserList implements \VuFind\ContentBlock\ContentBlockInterface
+{
+    /**
+     * Search class ID to use for retrieving facets.
+     *
+     * @var string
+     */
+    protected $searchClassId = 'Favorites';
+
+    /**
+     * User list id
+     *
+     * @var string
+     */
+    protected $listId;
+
+    /**
+     * Search runner
+     *
+     * @var \VuFind\Search\SearchRunner
+     */
+    protected $runner;
+
+    /**
+     * Constructor
+     *
+     */
+    public function __construct(\VuFind\Search\SearchRunner $runner) {
+        $this->runner = $runner;
+    }
+
+    protected function getUserList() {
+        return $this->runner->run(['id' => $this->listId], $this->searchClassId);
+    }
+    /**
+     * @inheritDoc
+     */
+    public function setConfig($settings)
+    {
+        $this->listId = $settings;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getContext()
+    {
+        return [
+            'listId' => $this->listId,
+            'results' => $this->getUserList(),
+            'searchClassId' => $this->searchClassId,
+        ];
+    }
+}
---
+++ module/KnihovnyCz/src/KnihovnyCz/Db/Table/WidgetContent.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * Class WidgetContent
+ *
+ * PHP version 7
+ *
+ * Copyright (C) Moravian Library 2019.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * @category VuFind
+ * @package  KnihovnyCz\Db\Table
+ * @author   Josef Moravec <moravec@mzk.cz>
+ * @license  https://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     https://knihovny.cz Main Page
+ */
+
+namespace KnihovnyCz\Db\Table;
+
+use Laminas\Db\Adapter\Adapter;
+use VuFind\Db\Table\PluginManager;
+use VuFind\Db\Row\RowGateway;
+
+class WidgetContent extends \VuFind\Db\Table\Gateway
+{
+    /**
+     * Constructor
+     *
+     * @param Adapter       $adapter Database adapter
+     * @param PluginManager $tm      Table manager
+     * @param array         $cfg     Zend Framework configuration
+     * @param RowGateway    $rowObj  Row prototype object (null for default)
+     * @param string        $table   Name of database table to interface with
+     */
+    public function __construct(Adapter $adapter, PluginManager $tm, $cfg,
+        RowGateway $rowObj = null, $table = 'widget_content'
+    ) {
+        parent::__construct($adapter, $tm, $cfg, $rowObj, $table);
+    }
+}
