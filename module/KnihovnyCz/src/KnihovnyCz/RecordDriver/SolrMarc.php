222d5f7b7052be429a7115c02512bd6e70b5ee91
Josef Moravec <josef.moravec@gmail.com>
2021-07-13 09:56:34 +0200

    Use refactored record drivers feature traits

--- module/KnihovnyCz/src/KnihovnyCz/RecordDriver/SolrMarc.php
+++ module/KnihovnyCz/src/KnihovnyCz/RecordDriver/SolrMarc.php
@@ -38,9 +38,9 @@ namespace KnihovnyCz\RecordDriver;
  */
 class SolrMarc extends SolrDefault
 {
-    use \VuFind\RecordDriver\IlsAwareTrait;
-    use \VuFind\RecordDriver\MarcReaderTrait;
-    use \VuFind\RecordDriver\MarcAdvancedTrait;
+    use \VuFind\RecordDriver\Feature\IlsAwareTrait;
+    use \VuFind\RecordDriver\Feature\MarcReaderTrait;
+    use \VuFind\RecordDriver\Feature\MarcAdvancedTrait;
     use PatentTrait;
 
     /**
