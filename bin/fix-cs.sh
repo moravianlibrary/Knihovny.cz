196ac4f7583f7ab6b8ea0dd0bef4716fdd7a9e0b
Josef Moravec <josef.moravec@gmail.com>
2021-08-10 10:20:23 +0200

    Fix service name

--- bin/fix-cs.sh
+++ bin/fix-cs.sh
@@ -3,6 +3,6 @@
 php-cs-fixer fix --config=tests/vufind.php_cs -vvv
 php-cs-fixer fix --config=tests/vufind_templates.php_cs -vvv
 
-CONTAINER=${1:-docker_vufind_1}
+CONTAINER=${1:-docker_vufind6_1}
 
 docker exec $CONTAINER vendor/bin/phing phpcbf
