a9f7485248f515c2a48860e9d55ddbff4b6e1a53
Věroš Kaplan <veros.kaplan@wftech.cz>
2021-09-11 00:43:44 +0200

    chore(image): add envsubst to the image

--- build/php-image/docker/bin/container-entrypoint.sh
+++ build/php-image/docker/bin/container-entrypoint.sh
@@ -1,29 +1,22 @@
 #!/bin/bash
 
+set -e
+set -x
+
 # read shibboleth private key from secrets
 if [ ! -f "/etc/shibboleth/sp-key.pem" ] && [ -f "/etc/secrets/sp-key.pem" ]; then
   ln -s  "/etc/secrets/sp-key.pem" "/etc/shibboleth/sp-key.pem"
 fi
 
 # configure shibboleth and apache
-files=("/etc/shibboleth/shibboleth2.xml" "/etc/apache2/sites-enabled/000-default.conf")
-for file in "${files[@]}"
-do
-  sed -i \
-    -e "s#\${PARAM_REGISTRATION_URL}#${PARAM_REGISTRATION_URL:-https:\/\/registrace.mzk.cz}#g" \
-    "$file"
-done
-
 if [ "${MEMCACHED_SERVICE}" = "" ]; then
-  export MEMCACHED_SERVICE=memcached:11211
+  export MEMCACHED_SERVICE=127.0.0.1:11211
 fi
 
-sed -i~ \
-    -e "s#\${MEMCACHED_SERVICE}#${MEMCACHED_SERVICE}#g" \
-    "/etc/shibboleth/shibboleth2.xml"
+envsubst.a8m -no-unset -i /etc/shibboleth/shibboleth2.xml.tmpl -o /etc/shibboleth/shibboleth2.xml
 
 # start Shibboleth or Apache
-if [ "$1" = "shibboleth" ]; then
+if [ "$1" = "shibd" -or "$1" = "shibboleth" ]; then
     exec shibd -f -F
 elif [ "$1" = "apache" ]; then
     exec apache2-foreground
--- build/php-image/docker/initscripts/bootstrap-shibboleth2.sh
+++ build/php-image/docker/initscripts/bootstrap-shibboleth2.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 set -e
+if [ -n "$DEBUG" ]; then
+  set -x
+fi
 
 exit 0
 
@@ -10,18 +13,16 @@ perror() {
 main() {
 
     local SHIB_CONF_DIR=/etc/shibboleth
-    local SHIB_CONF_TEMPL="shibboleth2.xml.templ"
+    local SHIB_CONF_TEMPL="shibboleth2.xml.tmpl"
     local SHIB_CONF="shibboleth2.xml"
 
     local SHIB_CONF_TEMPL_ABS_PATH="${SHIB_CONF_DIR}/${SHIB_CONF_TEMPL}"
-    if test ! -f "$SHIB_CONF_TEMPL_ABS_PATH"; then
-        perror "Shibboleth configuration could not be set! It's missing the template file '$SHIB_CONF_TEMPL_ABS_PATH'"
+    if [ ! -f "${SHIB_CONF_TEMPL_ABS_PATH}" ]; then
+        perror "Shibboleth configuration could not be set! It's missing the template file SHIB_CONF_TEMPL_ABS_PATH=${SHIB_CONF_TEMPL_ABS_PATH}"
         return 2
     fi
 
     local SHIB_CONF_ABS_PATH="${SHIB_CONF_DIR}/${SHIB_CONF}"
-
-    cp "$SHIB_CONF_TEMPL_ABS_PATH" "$SHIB_CONF_ABS_PATH" || (perror "Failed altering current configuration! Probably wrong permissions?" && return 3)
 
     if [ -z "$PARAM_VUFIND_HOST" ]; then
         PARAM_VUFIND_HOST="localhost"
@@ -42,27 +43,14 @@ main() {
         PARAM_VUFIND_ENTITY_ID=$PARAM_VUFIND_URL
     fi;
 
-    WAYF_FILE_URL="$PARAM_VUFIND_URL/Wayf"
+    export WAYF_FILE_URL="$PARAM_VUFIND_URL/Wayf"
     # Encode for URL parameter:
     WAYF_FILE_URL=$(echo "$WAYF_FILE_URL" | sed -e 's/%/%25/g' -e 's/ /%20/g' -e 's/!/%21/g' -e 's/"/%22/g' -e 's/#/%23/g' -e 's/\$/%24/g' -e 's/\&/%26/g' -e 's/'\''/%27/g' -e 's/(/%28/g' -e 's/)/%29/g' -e 's/\*/%2a/g' -e 's/+/%2b/g' -e 's/,/%2c/g' -e 's/-/%2d/g' -e 's/\./%2e/g' -e 's/\//%2f/g' -e 's/:/%3a/g' -e 's/;/%3b/g' -e 's//%3e/g' -e 's/?/%3f/g' -e 's/@/%40/g' -e 's/\[/%5b/g' -e 's/\\/%5c/g' -e 's/\]/%5d/g' -e 's/\^/%5e/g' -e 's/_/%5f/g' -e 's/`/%60/g' -e 's/{/%7b/g' -e 's/|/%7c/g' -e 's/}/%7d/g' -e 's/~/%7e/g')
 
-    sed -i \
-        -e "s#PARAM_VUFIND_HOST#${PARAM_VUFIND_HOST}#g" \
-        -e "s#PARAM_VUFIND_SSL_PORT#${PARAM_VUFIND_SSL_PORT}#g" \
-        -e "s#PARAM_VUFIND_URL#${PARAM_VUFIND_URL}#g" \
-        -e "s#PARAM_VUFIND_ENTITY_ID#${PARAM_VUFIND_ENTITY_ID}#g" \
-        -e "s#PARAM_SSL_DIR#${PARAM_SSL_DIR:-/etc/ssl/private}#g" \
-        -e "s#PARAM_SHIB_KEY_OUT#${PARAM_SHIB_KEY_OUT:-shibboleth2-sp-key.pem}#g" \
-        -e "s#PARAM_SHIB_CRT_OUT#${PARAM_SHIB_CRT_OUT:-shibboleth2-sp-cert.pem}#g" \
-        -e "s#WAYF_FILE_URL#${WAYF_FILE_URL}#g" \
-        "$SHIB_CONF_ABS_PATH" || return $?
+    exec envsubst.a8m -i ${SHIB_CONF_TEMPL_ABS_PATH} -o ${SHIB_CONF_ABS_PATH}
 
-    /usr/local/bin/generate_key.sh "$PARAM_SSL_DIR/$PARAM_SHIB_KEY_OUT" "$PARAM_SSL_DIR/$PARAM_SHIB_CRT_OUT" \
-        "$PARAM_VUFIND_HOST" "$PARAM_SSL_VALIDITY_DAYS" || return $?
-
-    service shibd stop &>/dev/null
-
-    service shibd start || (perror "Failed starting shibboleth daemon, something may be wrong with the configuration ..." && return 4)
+    #/usr/local/bin/generate_key.sh "$PARAM_SSL_DIR/$PARAM_SHIB_KEY_OUT" "$PARAM_SSL_DIR/$PARAM_SHIB_CRT_OUT" \
+    #    "$PARAM_VUFIND_HOST" "$PARAM_SSL_VALIDITY_DAYS" || return $?
 }
 
 main "$@"
