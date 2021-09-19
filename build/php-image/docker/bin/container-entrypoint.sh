c22e2cba3dfce6e1816dff4a43d8ca533c298696
Věroš Kaplan <veros.kaplan@wftech.cz>
2021-09-19 22:08:47 +0200

    fix(container-entrypoint): add content.local.ini

--- build/php-image/docker/bin/container-entrypoint.sh
+++ build/php-image/docker/bin/container-entrypoint.sh
@@ -26,6 +26,7 @@ fi
 envsubst.a8m -no-unset -i /etc/vufind/config.local.ini -o /var/www/knihovny-cz/local/knihovny.cz/config/vufind/config.local.ini
 envsubst.a8m -no-unset -i /etc/vufind/EDS.local.ini -o /var/www/knihovny-cz/local/knihovny.cz/config/vufind/EDS.local.ini
 envsubst.a8m -no-unset -i /etc/vufind/Search2.local.ini -o /var/www/knihovny-cz/local/knihovny.cz/config/vufind/Search2.local.ini
+envsubst.a8m -no-unset -i /etc/vufind/content.local.ini -o /var/www/knihovny-cz/local/knihovny.cz/config/vufind/content.local.ini
 
 # start Shibboleth or Apache
 if [ "$1" = "shibd" -o "$1" = "shibboleth" ]; then
