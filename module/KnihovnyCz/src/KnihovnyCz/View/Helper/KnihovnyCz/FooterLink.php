d24c9bc6e30e6726ae25c48e74dc1fcce64949f3
Robert Sipek <sipek@mzk.cz>
2021-09-21 14:49:42 +0200

    Issue 312: Implement footer links to portal pages

---
+++ module/KnihovnyCz/src/KnihovnyCz/View/Helper/KnihovnyCz/FooterLink.php
@@ -0,0 +1,63 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Class FooterLink
+ *
+ * PHP version 7
+ *
+ * Copyright (C) Moravian Library 2021.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * @category VuFind
+ * @package  KnihovnyCz\View\Helper\KnihovnyCz
+ * @author   Robert Sipek <sipek@mzk.cz>
+ * @license  https://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     https://knihovny.cz Main Page
+ */
+namespace KnihovnyCz\View\Helper\KnihovnyCz;
+
+use Laminas\View\Helper\AbstractHelper;
+
+/**
+ * Class FooterLink
+ *
+ * @category VuFind
+ * @package  KnihovnyCz\View\Helper\KnihovnyCz
+ * @author   Robert Sipek <sipek@mzk.cz>
+ * @license  https://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     https://knihovny.cz Main Page
+ */
+class FooterLink extends AbstractHelper
+{
+    /**
+     * Invoke
+     *
+     * @param string $page Page name
+     *
+     * @return string
+     */
+    public function __invoke(string $page): string
+    {
+        return $this->getView()->render(
+            'footer-link.phtml',
+            [
+                'page' => $page,
+                'title' => 'link_' . $page,
+            ]
+        );
+    }
+}
--- themes/KnihovnyCz/theme.config.php
+++ themes/KnihovnyCz/theme.config.php
@@ -6,10 +6,12 @@ return [
             'VuFind\View\Helper\Root\RecordDataFormatter' => 'KnihovnyCz\View\Helper\KnihovnyCz\RecordDataFormatterFactory',
         ],
         'invokables' => [
+            \KnihovnyCz\View\Helper\KnihovnyCz\FooterLink::class,
             \KnihovnyCz\View\Helper\KnihovnyCz\LibrariesApiLookfor::class,
             \KnihovnyCz\View\Helper\KnihovnyCz\SplitText::class,
         ],
         'aliases' => [
+            'footerLink' => \KnihovnyCz\View\Helper\KnihovnyCz\FooterLink::class,
             'recordDataFormatter' => 'VuFind\View\Helper\Root\RecordDataFormatter',
             'librariesApiLookfor' => \KnihovnyCz\View\Helper\KnihovnyCz\LibrariesApiLookfor::class,
             'splitText' => \KnihovnyCz\View\Helper\KnihovnyCz\SplitText::class,
