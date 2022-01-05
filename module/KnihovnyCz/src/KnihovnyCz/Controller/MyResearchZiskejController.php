c4cda609ac57f6d1447b7be90ae347e71d08105a
Vaclav Rosecky <xrosecky@gmail.com>
2022-01-05 09:02:17 +0100

    Issue 293: issues from testing resolved

--- module/KnihovnyCz/src/KnihovnyCz/Controller/MyResearchZiskejController.php
+++ module/KnihovnyCz/src/KnihovnyCz/Controller/MyResearchZiskejController.php
@@ -71,6 +71,7 @@ class MyResearchZiskejController extends AbstractBase
         if (!$this->getUser()) {
             return $this->forceLogin();
         }
+        $this->warnSocialUser();
         $view = $this->createViewModel();
         $view->setTemplate('myresearchziskej/list-all');
         return $view;
