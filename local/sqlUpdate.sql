5c98ca77de2d7d2a47752ebaaaf13d08d8edfc1b
Josef Moravec <josef.moravec@gmail.com>
2022-03-02 09:59:21 +0100

    Issue 501: Remove parameter server from citace.com API call

--- local/sqlUpdate.sql
+++ local/sqlUpdate.sql
@@ -1,3 +1,6 @@
 -- Add SQL update scripts here. Don't forget to add revision number after each script. Example below:
 -- UPDATE `system` SET `value` = '101' WHERE `key`='DB_VERSION';
 
+-- Issue 501
+DELETE FROM `config_items` WHERE (`name` = 'citation_local_domain');
+UPDATE `system` SET `value` = '101' WHERE `key`='DB_VERSION';
