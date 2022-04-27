e352aa25bee4be18081c9b59fe10d98b0cf15b3c
Josef Moravec <josef.moravec@mzk.cz>
2022-04-27 13:24:50 +0200

    Issue 551: Run QA check in container

---
+++ bin/clean_bad_symlinks.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+rm local/base/base \
+	 local/irel/irel \
+	 local/kiv/kiv \
+	 local/knihovny.cz/knihovny.cz \
+	 local/mus/mus \
+   local/mzk/mzk \
+	 local/tech/tech \
+   local/templates/templates
+
+rm module/KnihovnyCz/KnihovnyCz \
+   module/KnihovnyCzApi/KnihovnyCzApi \
+   module/KnihovnyCzConsole/KnihovnyCzConsole \
+   module/KnihovnyCzCronApi/KnihovnyCzCronApi
+
+rm themes/KnihovnyCz/KnihovnyCz \
+   themes/irel/irel \
+   themes/kiv/kiv \
+   themes/mus/mus \
+   themes/mzk/mzk \
+   themes/tech/tech
+
--- bin/qa.sh
+++ bin/qa.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-vendor/bin/phpstan analyse
-
-vendor/bin/php-cs-fixer fix --config=tests/vufind.php_cs --dry-run -vvv
-vendor/bin/php-cs-fixer fix --config=tests/vufind_templates.php_cs --dry-run -vvv
-
-eslint -c tests/.eslintrc.js themes/KnihovnyCz/js/*.js
-jshint --config=tests/jshint.json --exclude=themes/*/js/lib ./themes
-
 CONTAINER=${1:-docker_vufind6_1}
 
-docker exec $CONTAINER vendor/bin/phpunit
+docker exec $CONTAINER vendor/bin/phpstan --configuration=../knihovny-cz-extension/tests/phpstan.neon --memory-limit=2G analyse
+
+docker exec $CONTAINER vendor/bin/php-cs-fixer fix --config=../knihovny-cz-extension/tests/vufind.php_cs --dry-run -vvv
+docker exec $CONTAINER vendor/bin/php-cs-fixer fix --config=../knihovny-cz-extension/tests/vufind_templates.php_cs --dry-run -vvv
 docker exec $CONTAINER vendor/bin/phing phpcs-console
+
+docker exec $CONTAINER eslint -c .eslintrc.js ../knihovny-cz-extension/themes/KnihovnyCz/js/*.js
+docker exec $CONTAINER jshint --verbose --config=tests/jshint.json --exclude=../knihovny-cz-extension/themes/**/js/lib/ ../knihovny-cz-extension/themes
+
+docker exec $CONTAINER vendor/bin/phpunit
