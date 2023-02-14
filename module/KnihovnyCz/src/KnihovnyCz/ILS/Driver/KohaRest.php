3e6c6eb0e56b6c5a4e89ac47cc26e46023603b0d
Josef Moravec <josef.moravec@gmail.com>
2023-02-14 11:58:44 +0100

    Issue 786: Fix code style

--- module/KnihovnyCz/src/KnihovnyCz/ILS/Driver/KohaRest.php
+++ module/KnihovnyCz/src/KnihovnyCz/ILS/Driver/KohaRest.php
@@ -27,7 +27,6 @@ declare(strict_types=1);
  * @license  https://opensource.org/licenses/gpl-2.0.php GNU General Public License
  * @link     https://knihovny.cz Main Page
  */
-
 namespace KnihovnyCz\ILS\Driver;
 
 use VuFind\Exception\ILS as ILSException;
