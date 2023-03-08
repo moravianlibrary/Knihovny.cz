471467e35bb41c299b44b1ed638a330330e447db
Josef Moravec <josef.moravec@mzk.cz>
2023-03-08 08:21:47 +0100

    Issue 810: Add ILS status to admin

---
+++ module/KnihovnyCz/src/KnihovnyCz/Controller/AdminIlsController.php
@@ -0,0 +1,60 @@
+<?php
+declare(strict_types=1);
+/**
+ * Class Admin ILS driver controller
+ *
+ * PHP version 8
+ *
+ * Copyright (C) Moravian Library 2023.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * @category VuFind
+ * @package  KnihovnyCz\Admin
+ * @author   Josef Moravec <josef.moravec@mzk.cz>
+ * @license  https://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     https://knihovny.cz Main Page
+ */
+namespace KnihovnyCz\Controller;
+
+/**
+ * Class Admin ILS drivers controller
+ *
+ * @category VuFind
+ * @package  KnihovnyCz\Admin
+ * @author   Josef Moravec <josef.moravec@mzk.cz>
+ * @license  https://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     https://knihovny.cz Main Page
+ */
+class AdminIlsController extends \VuFindAdmin\Controller\AbstractAdmin
+{
+    /**
+     * Status action
+     *
+     * @return mixed
+     */
+    public function statusAction()
+    {
+        $config = $this->getConfig('MultiBackend')->Drivers;
+        $config = array_filter(
+            $config->toArray(),
+            function ($driver) {
+                return $driver !== 'NoILS';
+            }
+        );
+        $view = $this->createViewModel(['drivers' => $config]);
+        $view->setTemplate('admin/ils/status');
+        return $view;
+    }
+}
