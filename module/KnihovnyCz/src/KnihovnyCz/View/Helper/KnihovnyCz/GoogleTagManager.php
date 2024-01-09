9eb99c4b5af548d7af29d95f09139823dfc05ec9
Robert Šípek <sipek@mzk.cz>
2024-01-09 12:02:54 +0100

    Issue 1027: Remove duplicate GTM code

--- module/KnihovnyCz/src/KnihovnyCz/View/Helper/KnihovnyCz/GoogleTagManager.php
+++ module/KnihovnyCz/src/KnihovnyCz/View/Helper/KnihovnyCz/GoogleTagManager.php
@@ -16,32 +16,6 @@ namespace KnihovnyCz\View\Helper\KnihovnyCz;
 class GoogleTagManager extends \VuFind\View\Helper\Root\GoogleTagManager
 {
     /**
-     * Returns GTM Javascript code.
-     *
-     * @return string
-     */
-    protected function getRawJavascript(): string
-    {
-        return <<<JS
-            (function (w, d, s, l, i) {
-                  w[l] = w[l] || [];
-                  w[l].push({
-                    'gtm.start': new Date().getTime(),
-                    event: 'gtm.js'
-                  });
-                  var f = d.getElementsByTagName(s)[0],
-                      j = d.createElement(s),
-                      dl = l != 'dataLayer' ? '&l=' + l : '';
-                  j.async = true;
-                  j.src = '//www.googletagmanager.com/gtm.js?id=' + i + dl;
-                  var n = d.querySelector('[nonce]');
-                  n && j.setAttribute('nonce', n.nonce || n.getAttribute('nonce'));
-                  f.parentNode.insertBefore(j, f);
-                })(window, document, 'script', 'dataLayer', '{$this->gtmContainerId}');
-            JS;
-    }
-
-    /**
      * Is GTM enabled
      *
      * @return bool
@@ -49,41 +23,5 @@ class GoogleTagManager extends \VuFind\View\Helper\Root\GoogleTagManager
     public function isEnabled(): bool
     {
         return !empty($this->gtmContainerId);
-    }
-
-    /**
-     * Returns GTM Javascript code.
-     *
-     * @return string
-     */
-    public function getCode(): string
-    {
-        if (!$this->isEnabled()) {
-            return '';
-        }
-        $inlineScriptHelper = $this->getView()->plugin('inlinescript');
-        return $inlineScriptHelper(
-            \Laminas\View\Helper\HeadScript::SCRIPT,
-            $this->getRawJavascript(),
-            'SET'
-        )->toString();
-    }
-
-    /**
-     * Returns GTM noscript alternative code.
-     *
-     * @return string
-     */
-    public function getNoScriptCode(): string
-    {
-        if (!$this->isEnabled()) {
-            return '';
-        }
-        return <<<HTML
-            <noscript>
-              <iframe src="https://www.googletagmanager.com/ns.html?id={$this->gtmContainerId}"
-                      height="0" width="0" style="display:none;visibility:hidden"></iframe>
-            </noscript>
-            HTML;
     }
 }
