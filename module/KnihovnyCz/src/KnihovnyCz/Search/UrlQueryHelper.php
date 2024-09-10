6ee0c100610ae8b6f0167855b928f66c5efc7368
Vaclav Rosecky <vaclav.rosecky@mzk.cz>
2024-09-10 11:03:30 +0200

    Issue 1236: temporary workaround for AJAX facet filters

--- module/KnihovnyCz/src/KnihovnyCz/Search/UrlQueryHelper.php
+++ module/KnihovnyCz/src/KnihovnyCz/Search/UrlQueryHelper.php
@@ -35,4 +35,46 @@ class UrlQueryHelper extends \VuFind\Search\UrlQueryHelper
         $params['filter'] = $newFilters;
         return new static($params, $this->queryObject, $this->config, false);
     }
+
+    /**
+     * Get the current search parameters as a GET query.
+     *
+     * @param bool $escape Should we escape the string for use in the view?
+     * @param bool $number Number duplicated parameters
+     *
+     * @return string
+     */
+    public function getParams($escape = true, bool $number = false)
+    {
+        return '?' . static::buildQueryString($this->urlParams, $escape, $number);
+    }
+
+    /**
+     * Turn an array into a properly URL-encoded query string. This is
+     * equivalent to the built-in PHP http_build_query function, but it handles
+     * arrays in a more compact way and ensures that ampersands don't get
+     * messed up based on server-specific settings.
+     *
+     * @param array $a      Array of parameters to turn into a GET string
+     * @param bool  $escape Should we escape the string for use in the view?
+     * @param bool  $number Number duplicated parameters
+     *
+     * @return string
+     */
+    public static function buildQueryString($a, $escape = true, bool $number = false)
+    {
+        $parts = [];
+        foreach ($a as $key => $value) {
+            if (is_array($value)) {
+                foreach ($value as $index => $current) {
+                    $ord = ($number) ? $index : '';
+                    $parts[] = urlencode($key . '[' . $ord . ']') . '=' . urlencode($current ?? '');
+                }
+            } else {
+                $parts[] = urlencode($key) . '=' . urlencode($value ?? '');
+            }
+        }
+        $retVal = implode('&', $parts);
+        return $escape ? htmlspecialchars($retVal) : $retVal;
+    }
 }
