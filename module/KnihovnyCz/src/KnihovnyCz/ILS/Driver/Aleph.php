d4b0b5c82644ba25941b1a22a748d102cbe1ab9f
Rosecký Václav <vaclav.rosecky@mzk.cz>
2026-07-07 06:02:56 +0000

    Resolve "Zbytečná volání Alephu při každém zobrazení stránky"

--- module/KnihovnyCz/src/KnihovnyCz/ILS/Driver/Aleph.php
+++ module/KnihovnyCz/src/KnihovnyCz/ILS/Driver/Aleph.php
@@ -92,6 +92,26 @@ class Aleph extends AlephBase
     }
 
     /**
+     * Patron Login
+     *
+     * This is responsible for authenticating a patron against the catalog.
+     *
+     * @param string $user     The patron username
+     * @param string $password The patron's password
+     *
+     * @throws ILSException
+     * @return mixed          Associative array of patron info on successful login,
+     * null on unsuccessful login.
+     */
+    public function patronLogin($user, $password)
+    {
+        return [
+            'id' => $user,
+            'cat_username' => $user,
+        ];
+    }
+
+    /**
      * Helper method to determine whether or not a certain method can be
      * called on this driver.  Required method for any smart drivers.
      *
